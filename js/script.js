2b6873ebe387f11515aee25bcb63086efce482ea
Katherine <freepthanq1@gmail.com>
2025-08-22 23:23:30 +0300

    add noscroll for open menu

--- js/script.js
+++ js/script.js
@@ -4,4 +4,5 @@ const items = document.querySelector('.navbar__items');
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     items.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
 });
