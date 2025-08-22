7104c27415d2f4e86e56f084c5b56fcaacf2c675
Katherine <freepthanq1@gmail.com>
2025-08-22 16:09:24 +0300

    added toogle active

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+const burger = document.querySelector('.navbar__burger');
+const items = document.querySelector('.navbar__items');   
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    items.classList.toggle('active');
+});
