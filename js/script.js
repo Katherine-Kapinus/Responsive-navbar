c0b1b2f3e5d49020ed4ca5b1b690572fc5b43c3d
Katherine <freepthanq1@gmail.com>
2025-08-23 17:01:39 +0300

    js is ready

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ burger.addEventListener('click', () => {
     items.classList.toggle('active');
     document.body.classList.toggle('no-scroll');
 });
+
+let tabs = document.querySelectorAll('.item-tabs__title');
+let tabsContents = document.querySelectorAll('.tabs-content__body');
+console.log(tabs, tabsContents);
+tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+        tabsContents.forEach((content) => {
+            content.classList.remove('active');
+        })
+        tabs.forEach((tab) => {
+            tab.classList.remove('active');
+        })
+        tabsContents[index].classList.add('active');
+        tabs[index].classList.add('active');
+    })
+})
